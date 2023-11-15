18cbcb1b7d562e9afed1318d4928a539c676b1f6
HugolinBouhineau <hugolin.bc@gmail.com>
2023-11-15 10:33:35 +0100

    change starting positions

--- src/components/navigation/navigationArea.js
+++ src/components/navigation/navigationArea.js
@@ -4,20 +4,21 @@ import CasualFlapMapImageUrl from "/CasualFlatMap.png";
 function setupNavigationAreaGeometry() {
     // create occluder material
     const occluderMaterial = new MeshStandardMaterial({ color: 0xffc990 });
+/*     const occluderMaterial2 = new MeshStandardMaterial({ color: 0xeb4034 });
     //occluderMaterial.colorWrite = false;
 
     // create room map
     const navigationArea = new Group();
-/*     navigationArea.add(createWallElement(new Vector3(-4.85, 1, -0.74), new Vector3(0, 0, 0), new Vector3(0.0625, 3, 1.578), occluderMaterial));
-    navigationArea.add(createWallElement(new Vector3(-2.98, 1, -2.65), new Vector3(0, 0, 0), new Vector3(0.0625, 3, 3.51), occluderMaterial));
-    navigationArea.add(createWallElement(new Vector3(1, 1, -2.55), new Vector3(0, 0, 0), new Vector3(0.0625, 3, 3.467), occluderMaterial));
-    navigationArea.add(createWallElement(new Vector3(1, 1, 2.18), new Vector3(0, 0, 0), new Vector3(0.0625, 3, 4.475), occluderMaterial));
-    navigationArea.add(createWallElement(new Vector3(-0.689, 1, 0), new Vector3(0, 0, 0), new Vector3(8.518, 3, 0.06), occluderMaterial));
-    navigationArea.add(createWallElement(new Vector3(0.97, 1, -4.05), new Vector3(0, 0, 0), new Vector3(7.91, 3, 0.06), occluderMaterial));
-    navigationArea.add(createWallElement(new Vector3(-3.34, 1, -1.29), new Vector3(0, 0, 0), new Vector3(0.86, 3, 0.06), occluderMaterial));
-    navigationArea.add(createWallElement(new Vector3(4.86, 1, -0.01), new Vector3(0, 0, 0), new Vector3(0.06, 3, 9.114), occluderMaterial));
-    navigationArea.add(createWallElement(new Vector3(-1.6, 1, -0.88), new Vector3(0, 0, 0), new Vector3(2.85, 3, 0.06), occluderMaterial));
-    navigationArea.add(createWallElement(new Vector3(2.9, 1, 4.06), new Vector3(0, 0, 0), new Vector3(4, 3, 0.06), occluderMaterial));
+    navigationArea.add(createWallElement(new Vector3(-4.85, 1, -0.74), new Vector3(0, 0, 0), new Vector3(0.0625, 3, 1.578), occluderMaterial2));
+    navigationArea.add(createWallElement(new Vector3(-2.98, 1, -2.65), new Vector3(0, 0, 0), new Vector3(0.0625, 3, 3.51), occluderMaterial2));
+    navigationArea.add(createWallElement(new Vector3(1, 1, -2.55), new Vector3(0, 0, 0), new Vector3(0.0625, 3, 3.467), occluderMaterial2));
+    navigationArea.add(createWallElement(new Vector3(1, 1, 2.18), new Vector3(0, 0, 0), new Vector3(0.0625, 3, 4.475), occluderMaterial2));
+    navigationArea.add(createWallElement(new Vector3(-0.689, 1, 0), new Vector3(0, 0, 0), new Vector3(8.518, 3, 0.06), occluderMaterial2));
+    navigationArea.add(createWallElement(new Vector3(0.97, 1, -4.05), new Vector3(0, 0, 0), new Vector3(7.91, 3, 0.06), occluderMaterial2));
+    navigationArea.add(createWallElement(new Vector3(-3.34, 1, -1.29), new Vector3(0, 0, 0), new Vector3(0.86, 3, 0.06), occluderMaterial2));
+    navigationArea.add(createWallElement(new Vector3(4.86, 1, -0.01), new Vector3(0, 0, 0), new Vector3(0.06, 3, 9.114), occluderMaterial2));
+    navigationArea.add(createWallElement(new Vector3(-1.6, 1, -0.88), new Vector3(0, 0, 0), new Vector3(2.85, 3, 0.06), occluderMaterial2));
+    navigationArea.add(createWallElement(new Vector3(2.9, 1, 4.06), new Vector3(0, 0, 0), new Vector3(4, 3, 0.06), occluderMaterial2));
  */
     //1
     navigationArea.add(createWallElement(new Vector3(0,1.5,3.5), new Vector3(0, 0, 0), new Vector3(0.1,3,7), occluderMaterial));
